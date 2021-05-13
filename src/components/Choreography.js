a277676eb3f27495be69f209364a630e8dbc63d5
Joel Rudin <jarud55@gmail.com>
2021-05-13 16:02:45 -0400

    revive font, tweak captions

--- src/components/Choreography.js
+++ src/components/Choreography.js
@@ -3,12 +3,9 @@ import PhotoDisplay from './PhotoDisplay';
 import React, { useState } from 'react';
 import { SRLWrapper } from 'simple-react-lightbox';
 import { Container, Col } from 'react-bootstrap';
-// import ReactPlayer from "react-player";
-// import Iframe from 'react-iframe'
 
 const Choreography = () => {
   const [choreoImages] = useState([
-  
     {
       id: 20,
       link: 'images/choreography/CompaniaLisanormal.jpg',
@@ -20,32 +17,33 @@ const Choreography = () => {
       link: 'images/choreography/Grndsculpture_Piedraslrg.jpg',
       thumbnail: 'images/choreography/Grndsculpture_Piedras1.gif',
       title:
-        "Lisa's choreography, 'Al Ritmo de las  Piedras',(Rock Dance) at The Grounds for Sculpture, NJ",
+        'Lisa\'s choreography, "Al Ritmo de las Piedras",(Rock Dance) at The Grounds for Sculpture, NJ',
     },
     {
       id: 22,
       link: 'images/choreography/Sueno_Verdiales.jpg',
       thumbnail: 'images/choreography/Verdialis_smll copy.gif',
-      title: 'Lisa Choreography: The "Verdiales" was a powerhouse of community. (Robert Johnson, Star Ledger)',
+      title:
+        "Lisa's Choreography: \"The 'Verdiales' was a powerhouse of community.\" (Robert Johnson, Star Ledger)",
     },
     {
       id: 23,
       link: 'images/choreography/Lisacomedypiece.jpg',
       thumbnail: 'images/choreography/Lisacomedypiecesmll copy.gif',
-      title: '"Lisa Botalico and Eva Lucena stole the show with their comedic, \'Tanguillos por Chufla\'. (Damaris Solis, Flamenco Buzz)"',
-      
+      title:
+        '"Lisa Botalico and Eva Lucena stole the show with their comedic, \'Tanguillos por Chufla\'." (Damaris Solis, Flamenco Buzz)',
     },
     {
       id: 24,
       link: 'images/choreography/CarmenMedia_002.JPG',
       thumbnail: 'images/choreography/CarmenMedia_002.JPG',
-      title: "Roxey Ballet production of 'Carmen'",
+      title: 'Roxey Ballet production of "Carmen"',
     },
     {
       id: 25,
       link: 'images/choreography/CarmenMedia_018.JPG',
       thumbnail: 'images/choreography/CarmenMedia_018.JPG',
-      title: "Roxey Ballet production of 'Carmen'",
+      title: 'Roxey Ballet production of "Carmen"',
     },
     {
       id: 26,
@@ -57,7 +55,7 @@ const Choreography = () => {
       id: 27,
       link: 'images/choreography/DSC_4334.JPG',
       thumbnail: 'images/choreography/DSC_4334.JPG',
-      title: "'Fire Dance' from Manuel deFalla's 'El Amor Brujo'",
+      title: '"Fire Dance" from Manuel deFalla\'s "El Amor Brujo"',
     },
     {
       id: 28,
@@ -78,20 +76,21 @@ const Choreography = () => {
       link: 'images/choreography/Photo-0035.jpg',
       thumbnail: 'images/choreography/Lisa_Willie copy.gif',
       title:
-        'Willie Sanchez of NJN: "Images/Imagenes" and the Emmy for his documentary "The Spanish Guitar" with Lisa, featured dancer',
+        'Willie Sanchez of NJN\'s "Images/Imagenes" and the Emmy for his documentary "The Spanish Guitar" with Lisa, featured dancer',
     },
     {
       id: 31,
       link: 'images/choreography/Lisa_Ramya2.jpg',
       thumbnail: 'images/choreography/Lisa_Ramya2.jpg',
-      title: 'With Bharathanatyam dancer Ramya Ramnarayan: "...creating an atmosphere of moonlit rapture." (Star Ledger)',
+      title:
+        'With Bharathanatyam dancer Ramya Ramnarayan: "...creating an atmosphere of moonlit rapture." (Star Ledger)',
     },
     {
       id: 32,
       link: 'images/choreography/Lisa MarieGrnds for Sculpture3.jpg',
       thumbnail: 'images/choreography/Lisa MarieGrnds for Sculpture3.gif',
       title:
-        "'...en el jardin'with modern dancer Marie Alonzo at the Grounds for Sculpture, NJ",
+        '"...en el jardin" with modern dancer Marie Alonzo at the Grounds for Sculpture, NJ',
     },
     {
       id: 33,
@@ -110,7 +109,7 @@ const Choreography = () => {
       link: 'images/choreography/LisaCapture__Tarantula_best.JPG',
       thumbnail: 'images/choreography/LisaCapture__Tarantula_smll copy.gif',
       title:
-        "Performing 'Tarantula' with members of the New Brunswick Chamber Orchestra",
+        'Performing "Tarantula" with members of the New Brunswick Chamber Orchestra',
     },
     {
       id: 36,
@@ -130,20 +129,20 @@ const Choreography = () => {
       id: 38,
       link: 'images/choreography/Sueno_smll.jpg',
       thumbnail: 'images/choreography/Sueno_smll copy.gif',
-      title: "'Sueño', a Flamenco Nutcracker conceived and directed by Lisa.",
+      title: '"Sueño", a Flamenco Nutcracker conceived and directed by Lisa.',
     },
     {
       id: 39,
       link: 'images/Lisa and CristinaA11 cropped Nutwrk.jpg',
       thumbnail: 'images/Lisa and CristinaA11 cropped Nutwrk.jpg',
       title:
-        "Majas from 'Sueño', a Flamenco Nutcracker conceived and directed by Lisa.",
+        'Majas from "Sueño", a Flamenco Nutcracker conceived and directed by Lisa.',
     },
     {
       id: 40,
       link: 'images/choreography/lisajoel.jpg',
       thumbnail: 'images/choreography/lisajoelcropped.gif',
-      title: "'Anda Jaleo' with violist Joel Rudin and guitarist Ivan Max",
+      title: 'Singing "Anda Jaleo" with violist Joel Rudin and guitarist Ivan Max',
     },
     {
       id: 41,
@@ -156,28 +155,18 @@ const Choreography = () => {
       id: 51,
       link: 'images/choreography/Ramya_Lisa_The HinduArticle.jpg',
       thumbnail: 'images/choreography/Ramya_Lisa_The HinduArticle.jpg',
-      title:
-        'Article in The Hindu',
-    }
-   
-  
-  ])
-  
+      title: 'Article in The Hindu, India',
+    },
+  ]);
 
   return (
     <Container className='fluid choreography'>
       <h1>Choreography/Collaborations</h1>
       <Col xs={10}>
-      
         <SimpleReactLightbox>
-      
           <SRLWrapper>
-            <PhotoDisplay choreoImages={ choreoImages }/>
-           
-           
-    
+            <PhotoDisplay choreoImages={choreoImages} />
           </SRLWrapper>
-     
         </SimpleReactLightbox>
       </Col>
     </Container>
