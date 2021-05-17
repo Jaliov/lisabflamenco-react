a5149e44d6eeca8582df978f53e3634b2998e423
Joel Rudin <jarud55@gmail.com>
2021-05-17 10:47:35 -0400

    gitignore change

---
+++ build/precache-manifest.dd5d93274e735333b671b15d224c773d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c5732edfba235dab900",
+    "url": "/static/css/main.960b49cc.chunk.css"
+  },
+  {
+    "revision": "3c5732edfba235dab900",
+    "url": "/static/js/main.ccb220c8.chunk.js"
+  },
+  {
+    "revision": "eb2f7dd5b8d492ff4016",
+    "url": "/static/js/runtime~main.65b27f6d.js"
+  },
+  {
+    "revision": "7040c5ff55c55ecdc4be",
+    "url": "/static/css/2.5aa15deb.chunk.css"
+  },
+  {
+    "revision": "7040c5ff55c55ecdc4be",
+    "url": "/static/js/2.0b900d39.chunk.js"
+  },
+  {
+    "revision": "666eb2b96bdfcfe59711",
+    "url": "/static/js/3.034e91e1.chunk.js"
+  },
+  {
+    "revision": "fcc079637d6249327f04c7700bfb939d",
+    "url": "/static/media/ShawlintheAirfade.fcc07963.png"
+  },
+  {
+    "revision": "16240efd41419fad2972219854ed0fda",
+    "url": "/static/media/cubanlungefade.16240efd.png"
+  },
+  {
+    "revision": "8ec4aa76c9ebd2c613eb8b2fa11a89f3",
+    "url": "/static/media/lisakiddiesedit.8ec4aa76.jpg"
+  },
+  {
+    "revision": "33d44fb70f65c802c9eaa12725bd492d",
+    "url": "/static/media/IMG_0473.33d44fb7.jpg"
+  },
+  {
+    "revision": "46065efdd8843e85aaa73d41640c1f98",
+    "url": "/static/media/blackwhitepolkaadotglow.46065efd.png"
+  },
+  {
+    "revision": "a64988516c81cf97c5fbcaa0b98fb9d8",
+    "url": "/static/media/Lisamarie4fade.a6498851.png"
+  },
+  {
+    "revision": "34a1156588649c61ea04538baaeef237",
+    "url": "/static/media/MATURASC_1.34a11565.TTF"
+  },
+  {
+    "revision": "8afb05070103ce20ba99a85c5e85b994",
+    "url": "/static/media/Reviewbackground3.8afb0507.png"
+  },
+  {
+    "revision": "315cd1618c0c6997f1f16382a57547ae",
+    "url": "/index.html"
+  }
+];
