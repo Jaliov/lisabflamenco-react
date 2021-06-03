a53f6c6fff595f9a47b30c473324a1b49be17ea2
Joel Rudin <jarud55@gmail.com>
2021-06-03 13:06:32 -0400

    fix Press copy

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -14,7 +14,7 @@ const Reviews = () => {
           <Carousel.Caption>
             <h3>New York Times</h3>
             <p className='larger'>
-              "Another notable solo was Lisa Botalico's <em>'Solea'</em> which,
+              "Another notable solo was Lisa Botalico's <em>Solea</em> which,
               began as a lamentation and proceeded into a dance of pride and
               defiance."
               <em>
@@ -27,12 +27,9 @@ const Reviews = () => {
 
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
-            <h3>New Jersey Star Ledger</h3>
             <p className='larger'>
               "Botalico passed through a variety of rhythms and moods in her
-              solo
-              <em>'Cantinas.'</em>
-              In sequence, she emphasized the swaying of her hips, the curling
+              solo <em>Cantinas.</em> In sequence, she emphasized the swaying of her hips, the curling
               tracery of her arms, starkly dramatic poses and a rapid
               'zapateado.' Finally, hitching up her skirts, she became a saucy
               one woman fiesta."
@@ -42,11 +39,11 @@ const Reviews = () => {
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
-            <h3>Asbury Park Press</h3>
+           
             <p className='larger'>
               "Lisa Botalico was the essence of fiery passion, <br />
               first playing the role of a slyly sultry seductress, then becoming
-              the personification of a flickering flame."
+              the personification of a flickering flame." <em>(Asbury Park Press)</em>
               <br />
             </p>
           </Carousel.Caption>
@@ -55,7 +52,7 @@ const Reviews = () => {
           <Carousel.Caption>
             <h3>Il Progresso</h3>
             <p className='larger'>
-              "Bravo Lisa Bottalico, in an <em>applauditissimo 'Solea'</em>; she
+              "Bravo Lisa Bottalico, in an applauditissimo <em>Solea</em>; she
               made the stage tremble."
               <br />
               <em>(Mario Fratti)</em>
@@ -64,17 +61,16 @@ const Reviews = () => {
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
-            <h3>Asbury Park Press</h3>
             <p className='larger'>
-              "...Lisa Botalico's 'Solea' had the crowd on it's feet, clapping
+              "...Lisa Botalico's <em>Solea</em> had the crowd on it's feet, clapping
               in rhythm because of the audacious display of drama emanating from
-              her being." (Asbury Park Press)
+              her being." <em>(Asbury Park Press)</em>
             </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
-            <h3>Princeton Town Topics (link)</h3>
+            <h3>Princeton Town Topics <small>(link)</small></h3>
             <p className='larger'>
               <a
                 href='http://www.towntopics.com/wordpress/2015/10/14/ole-flamenco-flourishes-in-princeton-as-lisa-botalico-leads-eager-spanish-dancers/'
