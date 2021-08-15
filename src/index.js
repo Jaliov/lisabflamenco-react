3a7bb6dc3c7b42a297defe27dbc2db93138e4086
Joel Rudin <jarud55@gmail.com>
2021-08-15 17:43:52 -0400

    eliminate snap

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SimpleReactLightbox from 'simple-react-lightbox';
-
-const rootElement = document.getElementById('root');
+import SimpleReactLightbox from 'simple-react-lightbox'
 
 ReactDOM.render(
   <React.StrictMode>
-    <SimpleReactLightbox>
-      <App />
+     <SimpleReactLightbox>
+    <App />
     </SimpleReactLightbox>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
