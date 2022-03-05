109b4482885648376dc16af8d364c32a87e6586e
Joel Rudin <jarud55@gmail.com>
2022-03-05 13:36:05 -0500

    insta reformat

--- src/components/VideoGallery.js
+++ src/components/VideoGallery.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import {Helmet} from "react-helmet";
-import ReactPlayer from 'react-player';
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import Iframe from 'react-iframe';
-import InstagramEmbed from 'react-instagram-embed';
+import React from "react";
+import { Helmet } from "react-helmet";
+import ReactPlayer from "react-player";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import Iframe from "react-iframe";
+import InstagramEmbed from "react-instagram-embed";
 const cardStyle = {
-  width: '100%',
-  height: '350px',
-  boxShadow: '5px 3px 3px rgb(217, 83, 79, 0.5)',
-  marginBottom: '5%',
+  width: "100%",
+  height: "350px",
+  boxShadow: "5px 3px 3px rgb(217, 83, 79, 0.5)",
+  marginBottom: "5%",
 };
-const vidUrl1 = 'https://www.youtube.com/embed/Wwou3GZ4tZ8';
+const vidUrl1 = "https://www.youtube.com/embed/Wwou3GZ4tZ8";
 const vidUrl2 =
-  'https://drive.google.com/file/d/1jwRTAuRNkzQic8W-n1sLDLakh0KD3zux/preview';
-  const vidUrl3 =
-  'https://www.instagram.com/p/CUNg9ulA-fl/?utm_source=ig_web_button_share_sheet/embed';
+  "https://drive.google.com/file/d/1jwRTAuRNkzQic8W-n1sLDLakh0KD3zux/preview";
+const vidUrl3 =
+  "https://www.instagram.com/p/CUNg9ulA-fl/?utm_source=ig_web_button_share_sheet/embed";
 
-  
 const VideoGallery = () => {
   return (
     <>
-     <Helmet>
+      <Helmet>
         <title>Videos</title>
-          <meta name = "description" content = "Check out these Awesome Videos of Lisa Botalico!" />
-          </Helmet>
+        <meta
+          name="description"
+          content="Check out these Awesome Videos of Lisa Botalico!"
+        />
+      </Helmet>
       <Container>
         <h1>Videos!</h1>
-        <Row >
-          <Col md = {5}>
+        <Row>
+          <Col md={5}>
             <Card
-              className='bg-dark text-light'
+              className="bg-dark text-light"
               key={Iframe.id}
               style={cardStyle}
             >
@@ -37,9 +39,9 @@ const VideoGallery = () => {
                 url={vidUrl1}
                 width={cardStyle.width}
                 controls={true}
-                className='border border-secondary'
-                origin='window.location.host'
-                alt='Lisa as Juana la'
+                className="border border-secondary"
+                origin="window.location.host"
+                alt="Lisa as Juana la"
               />
               <Card.Body>
                 <Card.Title>
@@ -49,22 +51,26 @@ const VideoGallery = () => {
               </Card.Body>
             </Card>
           </Col>
-          <Col md={2}></Col>
-          <Col md = {5}>
+          </Row>
+          <Row>
+         
             <Card
-              className='bg-dark text-light'
+              className="bg-dark text-light"
               key={Iframe.id}
               style={cardStyle}
             >
-              <Iframe
-                url={vidUrl2}
-                alt='Lisa dancing Sevillanas por la noche'
-                title='Nighttime Sevillanas'
-                id='70'
-                width={cardStyle.width}
-                height={cardStyle.height}
-                className='border border-secondary'
-                allowFullScreen
+              <InstagramEmbed
+                url="https://www.instagram.com/alborada_dance/p/CUNg9ulA-fl/"
+                clientAccessToken="123|456"
+                maxWidth={600}
+                hideCaption={false}
+                containerTagName="div"
+                protocol=""
+                injectScript
+                onLoading={() => {}}
+                onSuccess={() => {}}
+                onAfterRender={() => {}}
+                onFailure={() => {}}
               />
 
               <Card.Body>
@@ -75,25 +81,11 @@ const VideoGallery = () => {
                 <Card.Text>Quarantine Project</Card.Text>
               </Card.Body>
             </Card>
-            <InstagramEmbed
-  url='https://www.instagram.com/alborada_dance/p/CUNg9ulA-fl/'
-  clientAccessToken='123|456'
-  maxWidth={320}
-  hideCaption={false}
-  containerTagName='div'
-  protocol=''
-  injectScript
-  onLoading={() => {}}
-  onSuccess={() => {}}
-  onAfterRender={() => {}}
-  onFailure={() => {}}
-/>
-          </Col>
-          </Row>
-          <p className='text-light larger'>
-            <em>More soon!</em>
-          </p>
-       
+        
+        </Row>
+        <p className="text-light larger">
+          <em>More soon!</em>
+        </p>
       </Container>
     </>
   );
