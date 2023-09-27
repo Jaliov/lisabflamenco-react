83f70f479932e53b276d6a1589edbb37d8d06546
Joel Rudin <jarud55@gmail.com>
2023-09-27 09:17:47 -0400

    upgrades

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -5,21 +5,9 @@ const AddEvent = () => {
   const addEvents = [
     {
       id: 46,
-      date: "Friday, June 9, 7 PM",
-      text: "Juerga Flamenca at Byron Barn, Plainsboro NJ",
-      link: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fbyron.barn%2Fposts%2Fpfbid0FZTrkHxoS1D5FqbW66enLZjMw6M53XBHiCRonZgQm1Ek9aAuWBwFBy3QzMhStxxBl&show_text=true&width=500",
-    },
-      {
-      id: 47,
-      date: "Sunday, June 11, 4 PM",
-      text: "Tablao Performances at Spain Inn 2 Restaurant, Asbury NJ",
-      link: "https://www.spaininn2.com/",
-    },
-    {
-      id: 48,
-      date: "Sunday, June 25 from 2:30-6 PM",
-      text: "Alborada's 10th Annual Feria de Sevilla, Park Press Park, Woodbridge",
-      link: "http://www.AlboradaDance.org/",
+      date: "More event listings coming soon!",
+      text: "",
+      link: "",
     },
   ];
   return addEvents ? (
@@ -27,7 +15,10 @@ const AddEvent = () => {
       <>
         <Card.Text key={event.id} className="text-warning">
           <p className="larger">
-            <span style={{ color: "yellow" }}> {event.date}: </span>
+            <span style={{ color: "yellow", fontWeight: "bold" }}>
+              {" "}
+              {event.date}:{" "}
+            </span>
           </p>{" "}
           <Card.Link href={event.link} target="blank">
             {" "}
--- src/components/PhotoLayout.js
+++ src/components/PhotoLayout.js
@@ -1,4 +1,4 @@
-const videolayout = { width: '1700px', height: '1000px', margin: '3px 6px' };
+const videolayout = { width: "1700px", height: "1000px", margin: "3px 6px" };
 const PhotoLayout = {
   videolayout,
 };
