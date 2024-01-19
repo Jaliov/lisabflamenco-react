5b08986a2359386511cb06605f93a9360d053e2f
Joel Rudin <jarud55@gmail.com>
2024-01-19 16:37:30 -0500

    homepage animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import {Helmet} from "react-helmet";
-import Homephoto from './Homephoto';
-import cover from '../assets/ShawlintheAirfade.png';
-import { Container, Row, Col } from 'react-bootstrap';
-
+import React from "react";
+import { Helmet } from "react-helmet";
+import Homephoto from "./Homephoto";
+import cover from "../assets/ShawlintheAirfade.png";
+import { Container, Row, Col } from "react-bootstrap";
 
 // import Button from 'react-bootstrap/Button';
 // import Modal from 'react-bootstrap/Modal';
@@ -12,34 +11,36 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Home = () => {
   return (
     <>
-    <Helmet>
-    <meta name="description" content="Discover Lisa Botalico, acclaimed Flamenco dancer, dance and music of Spain" />
-    </Helmet>
+      <Helmet>
+        <meta
+          name="description"
+          content="Discover Lisa Botalico, acclaimed Flamenco dancer, dance and music of Spain"
+        />
+      </Helmet>
       <Container>
-        
-         <Row>
-        <Col  md ={4}></Col>
-       <Col md ={4}>
-       <header>
-        <h1 className = 'home'>
-          <em>Lisa Botalico </em>
-        </h1>
-        <p className = 'larger text-light opening'>
-          <em>
-            ...danced and sang with her head  
-            thrown back, her energy seemingly unquenchable.{' '}<br />
-          </em>
-          <small>(Star Ledger)</small>
-        </p>
-        </header>
-       
-        <Homephoto cover={cover} />
-       
-        </Col>
-        <Col  md ={4}></Col>
+        <Row>
+          <Col md={4}></Col>
+          <Col md={4}>
+            <header>
+              <h1 className="home">
+                <em>Lisa Botalico </em>
+              </h1>
+              <p className="larger text-light opening">
+                <em>
+                  ...danced and sang with her head thrown back, her energy
+                  seemingly unquenchable. <br />
+                </em>
+                <small>(Star Ledger)</small>
+              </p>
+            </header>
+            <div class="fade-in-image">
+              <Homephoto cover={cover} />
+            </div>
+          </Col>
+          <Col md={4}></Col>
         </Row>
       </Container>
-      </>
+    </>
   );
 };
 export default Home;
