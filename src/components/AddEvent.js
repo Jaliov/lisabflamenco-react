bab893595290fa364ca6074365b799decc135dfe
Joel Rudin <jarud55@gmail.com>
2024-03-29 15:17:39 -0400

    add event April 9

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -5,20 +5,20 @@ const AddEvent = () => {
   const addEvents = [
     {
       id: 46,
-      date: "Feb 24 - Mar 3, 2024",
-      text: "Roxey Ballet Carmen",
+      date: "APRIL 9, 2024",
+      text: "PAUL ROBESON 126TH BIRTHDAY CELEBRATION",
       nonlink:
-        '"...the performance features a guest appearance by world-class flamenco dancer Lisa Botalico who embodies the role of the Fortune Teller. Don’t miss the chance to experience the passion and drama of Carmen, brought to life through the artistry of Roxey Ballet Company."',
-      link: "https://www.roxeyballet.org/carmen",
+        '"...This program is a multi-media celebration of the legacy of Paul Robeson, Princeton’s “native son” and scholar, all American athlete, artist, and activist. This celebration of his legacy is free and open to all ages, presented in partnership with the Witherspoon Jackson Historical and Cultural Society (WJHCS)."',
+      link: "https://artscouncilofprinceton.org/event/robeson-126/",
     },
-    {
+    /*{
       id: 47,
       date: "Friday, March 15, 8 pm; Sunday, March 17, 3 pm 2024",
       text: "Boheme Opera Carmen",
       nonlink:
         "Choreography by Lisa Botalico performed by Lisa with dancers from the Alborada Spanish Dance Theatre",
       link: "https://www.bohemeopera.org/carmen",
-    },
+    },*/
   ];
   return addEvents ? (
     addEvents.map((event) => (
