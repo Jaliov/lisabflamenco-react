534222fab4f1b5cc14950a92a91b953886e2de33
Joel Rudin <jarud55@gmail.com>
2024-05-19 21:12:43 -0400

    modal background, close button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
-
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import Iframe from "react-iframe";
 function Example() {
   const [show, setShow] = useState(true);
   const handleClose = () => setShow(false);
@@ -12,12 +9,11 @@ function Example() {
   return (
     <>
       <Modal show={show} onHide={handleClose} centered id="anniversary">
-        <img src="images/AnniversaryFlyer_4.jpg" id="anniversary"></img>{" "}
+        <img src="images/AnniversaryFlyer_4.jpg"></img>{" "}
         <Modal.Body>
           <i
             class="gg-close-r"
-            variant="danger"
-            style={{ color: "red" }}
+            style={{ color: "white" }}
             onClick={handleClose}
           ></i>
         </Modal.Body>
