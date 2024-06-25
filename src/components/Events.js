20e6238308e0a1f469a620c05b84254e18958a08
Joel Rudin <jarud55@gmail.com>
2024-06-25 08:55:55 -0400

    responsive events page

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Container, Row, Col, Card, Alert } from "react-bootstrap";
+import { Container, Row, Card, Alert } from "react-bootstrap";
 import AddEvent from "./AddEvent";
 import UsefulLinks from "./UsefulLinks";
 
@@ -14,35 +14,36 @@ const Events = () => {
           content="Don't miss out on Lisa Botalico's Upcoming Events!"
         />
       </Helmet>
-      <Container>
+      <Container className="container-fluid">
         <h1 class="bio" style={{ textAlign: "center" }}>
           Events/Links
         </h1>
         <Row>
-          <Col xs={8}>
-            <Alert variant={"transparent text-light bg-transparent"} id="alert">
-              For Information about Performances & Classes:{" "}
-              <Alert.Link href="mailto:lisasolea@aol.com"> email</Alert.Link>
-            </Alert>
-            l
-            <Card className="bg-transparent text-light">
-              <Card.Body>
-                <Card.Title className="text-light">Upcoming Events </Card.Title>
-                <AddEvent />
-              </Card.Body>
-            </Card>
-            <Card className="bg-transparent text-light">
-              <Card.Body>
-                <Card.Title>Important Links</Card.Title>
+          <Alert variant={"transparent text-light bg-transparent"} id="alert">
+            For Information about Performances & Classes:{" "}
+            <Alert.Link href="mailto:lisasolea@aol.com"> email</Alert.Link>
+          </Alert>
+          l
+          <Card className="bg-transparent text-light">
+            <Card.Body>
+              <Card.Title className="text-light  events">
+                Upcoming Events{" "}
+              </Card.Title>
+              <AddEvent />
+            </Card.Body>
+          </Card>
+          <Card className="bg-transparent text-light">
+            <Card.Body>
+              <Card.Title>Important Links</Card.Title>
 
-                <UsefulLinks />
-                {/* <p>
+              <UsefulLinks />
+              {/* <p>
                   <em style={{ color: '#FFFFCC' }}>
                     Lisa teaches students at Rutgers University, Feb, 2015.
                     Scroll to Page 5 for article below
                   </em>
                 </p> */}
-                {/* <div
+              {/* <div
                   data-configid='0/11648128'
                   style={{
                     marginTop: '0px',
@@ -54,30 +55,28 @@ const Events = () => {
                     className: 'issuuembed',
                   }}
                 ></div> */}
-              </Card.Body>
-            </Card>
+            </Card.Body>
+          </Card>
+          <br />
+          {/* Start of SimpleHitCounter Code */}
+          <div align="left">
+            <a href="https://www.free-website-hit-counter.com">
+              <img
+                src="https://www.free-website-hit-counter.com/c.php?d=9&id=148080&s=288"
+                border="0"
+                alt="Free Website Hit Counter"
+              ></img>
+            </a>
             <br />
-            {/* Start of SimpleHitCounter Code */}
-            <div align="left">
-              <a href="https://www.free-website-hit-counter.com">
-                <img
-                  src="https://www.free-website-hit-counter.com/c.php?d=9&id=148080&s=288"
-                  border="0"
-                  alt="Free Website Hit Counter"
-                ></img>
+            <small>
+              <a
+                href="https://www.free-website-hit-counter.com"
+                title="Free Website Hit Counter"
+              >
+                Free website hit counter
               </a>
-              <br />
-              <small>
-                <a
-                  href="https://www.free-website-hit-counter.com"
-                  title="Free Website Hit Counter"
-                >
-                  Free website hit counter
-                </a>
-              </small>
-            </div>
-          </Col>
-          <Col></Col>
+            </small>
+          </div>
           {/* End of SimpleHitCounter Code */}
         </Row>
       </Container>
