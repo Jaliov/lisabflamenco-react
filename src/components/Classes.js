510fad5521b85898557a631c5962cbd179a5a8bf
Joel Rudin <jarud55@gmail.com>
2024-07-14 11:59:52 -0400

    layout edits teaching page

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -32,52 +32,51 @@ const Classes = () => {
                 className="fade-in-image studentPhotos"
                 alt="Lisa teaching youngsters"
               />
-              <Card.Body>
-                <Card.Title className="text-light larger">
-                  {" "}
-                  Lisa teaches classes for adults and children of all ages and
-                  levels. <br />
-                  Private instruction also available.{" "}
-                </Card.Title>
-                <Card.Text className="alertyellow larger">
-                  {" "}
-                  Contact:
-                  <br />
-                  <Card.Link
-                    className="text-light"
-                    href="http://www.artscouncilofprinceton.org/"
-                    target="blank"
-                  >
-                    Arts Council of Princeton
-                  </Card.Link>
-                  <br />
-                  <Card.Link
-                    href="mailto:lisasolea@aol.com"
-                    className="text-light"
-                  >
-                    Email
-                  </Card.Link>
-                  <br />
-                  See videos of Lisa teaching on the{" "}
-                  <Card.Link href="/videogallery" className="text-light">
-                    Videos{" "}
-                  </Card.Link>
-                  page
-                </Card.Text>
-              </Card.Body>
+
+              <Card.Title className="text-light larger">
+                {" "}
+                Lisa teaches classes for adults and children of all ages and
+                levels. <br />
+                Private instruction also available.{" "}
+              </Card.Title>
+              <Card.Text className="alertyellow larger">
+                {" "}
+                Contact:
+                <br />
+                <Card.Link
+                  className="text-light"
+                  href="http://www.artscouncilofprinceton.org/"
+                  target="blank"
+                >
+                  Arts Council of Princeton
+                </Card.Link>
+                <br />
+                <Card.Link
+                  href="mailto:lisasolea@aol.com"
+                  className="text-light"
+                >
+                  Email
+                </Card.Link>
+                <br />
+                See videos of Lisa teaching on the{" "}
+                <Card.Link href="/videogallery" className="text-light">
+                  Videos{" "}
+                </Card.Link>
+                page
+              </Card.Text>
             </Card>
           </Col>
         </Row>
+
         <Row>
           <Col md={1} />
           <Col md={10}>
             <Card bg="transparent classMedia">
-              <Card.Body>
-                <Card.Title className="text-light larger">
-                  June 8, 2024: in recognition of 25 years teaching at The Arts
-                  Council of Princeton, Lisa has a studio named in her honor!
-                </Card.Title>
-              </Card.Body>
+              <Card.Title className="text-light larger">
+                June 8, 2024: in recognition of 25 years teaching at The Arts
+                Council of Princeton, Lisa has a studio named in her honor!
+              </Card.Title>
+
               <Card.Img
                 variant="bottom"
                 src={LisaStudentsAward}
@@ -87,29 +86,28 @@ const Classes = () => {
           </Col>
           {/* <Col xs={1}></Col> */}
         </Row>
-
+        <br />
         <Row>
           <Col md={1} />
           <Col md={10}>
             <Card bg="transparent classMedia">
-              <Card.Body>
-                <Card.Title className="text-light larger">
-                  Below:{" "}
-                  <em className="alertyellow larger">
-                    Fiesta del Día de Los Reyes Magos
-                  </em>{" "}
-                  or Three Kings Day, is celebrated throughout the world to mark
-                  the culmination of the 12 days of Christmas. On Saturday, Jan
-                  6, 2024, a dance performance by{" "}
-                  <span className="alertyellow">students</span> in the Arts
-                  Council of Princeton’s Flamenco program, led by Lisa Botalico,
-                  center, featured multiple dances as attendees learned about
-                  this cultural holiday. <br />
-                  <span className="smaller">
-                    <small>(Photo by Charles R. Plohn)</small>
-                  </span>
-                </Card.Title>
-              </Card.Body>
+              <Card.Title className="text-light larger">
+                Below:{" "}
+                <em className="alertyellow larger">
+                  Fiesta del Día de Los Reyes Magos
+                </em>{" "}
+                or Three Kings Day, is celebrated throughout the world to mark
+                the culmination of the 12 days of Christmas. On Saturday, Jan 6,
+                2024, a dance performance by{" "}
+                <span className="alertyellow">students</span> in the Arts
+                Council of Princeton’s Flamenco program, led by Lisa Botalico,
+                center, featured multiple dances as attendees learned about this
+                cultural holiday. <br />
+                <span className="smaller">
+                  <small>(Photo by Charles R. Plohn)</small>
+                </span>
+              </Card.Title>
+
               <Card.Img
                 variant="bottom"
                 src="https://www.towntopics.com/wordpress/wp-content/uploads/2024/01/page-1-1-10-24-web-1.jpg"
@@ -119,17 +117,16 @@ const Classes = () => {
           </Col>
           {/* <Col xs={1}></Col> */}
         </Row>
+        <br />
         <Row>
           <Col md={1} />
           <Col md={10}>
             <Card bg="transparent classMedia">
-              <Card.Body>
-                <Card.Title className="text-light larger">
-                  {" "}
-                  Lisa with students after the first in-person student showcase,
-                  post-lockdown, June, 2021
-                </Card.Title>
-              </Card.Body>{" "}
+              <Card.Title className="text-light larger">
+                {" "}
+                Lisa with students after the first in-person student showcase,
+                post-lockdown, June, 2021
+              </Card.Title>{" "}
               <Card.Img
                 src={LisaStudents}
                 className="studentPhotos"
