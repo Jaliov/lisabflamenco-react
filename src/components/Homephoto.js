0072dee153b64ce02b64f7b6aa2f77275c78355d
Joel Rudin <jarud55@gmail.com>
2024-07-28 18:24:30 -0400

    home media queries

--- src/components/Homephoto.js
+++ src/components/Homephoto.js
@@ -3,12 +3,13 @@ import React from "react";
 const Homephoto = ({ photo, cover }) => {
   photo = (
     <img
+      id="cover"
       src={cover}
       alt="cover"
       width="400px"
       height="600px"
       className="rounded mx-auto d-block"
-      style={{ backgroundSize: "400px, 600px" }}
+      style={{ backgroundSize: "25rem, 37.5rem" }}
       centered="true"
     ></img>
   );
