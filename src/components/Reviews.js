540c514a4d30f7bb5f290a788a02a9e78e74105e
Joel Rudin <jarud55@gmail.com>
2024-10-20 14:24:56 -0400

    Press page edits

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -12,28 +12,87 @@ const Reviews = () => {
       <Carousel className="press fade-in-image">
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
+            <h3>New York Times</h3>
+            <h6 className="larger">
+              "Another notable solo was Lisa Botalico's <em>Solea</em> which,
+              began as a lamentation and proceeded into a dance of pride and
+              defiance."
+              <em>
+                <br />
+                (Jack Anderson, NY Times)
+              </em>
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
+            <h6 className="larger">
+              "Botalico passed through a variety of rhythms and moods in her
+              solo <em>Cantinas.</em>...she emphasized the swaying of her hips,
+              the curling tracery of her arms, starkly dramatic poses and a
+              rapid 'zapateado.' Finally, hitching up her skirts, she became a
+              saucy one woman fiesta."
+              <em>(Star Ledger)</em>
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
+            <h6 className="larger">
+              "Lisa Botalico was the essence of fiery passion, first playing the
+              role of a slyly sultry seductress, then becoming the
+              personification of a flickering flame."{" "}
+              <em>(Asbury Park Press)</em>
+              <br />
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
+            <h3>Il Progresso</h3>
+            <h6 className="larger">
+              "Bravo Lisa Bottalico, in an applauditissimo <em>Solea</em>; she
+              made the stage tremble."
+              <br />
+              <em>(Mario Fratti)</em>
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
+            <h6 className="larger">
+              "...Lisa Botalico's <em>Solea</em> had the crowd on it's feet,
+              clapping in rhythm because of the audacious display of drama
+              emanating from her being." <em>(Asbury Park Press)</em>
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
             <h3>
-              Princeton Town Topics, 2024 <small>(link)</small>
+              Princeton Town Topics <small>(link)</small>
             </h3>
-            <p className="larger">
+            <h6 className="larger">
               <a
-                href="https://www.towntopics.com/wordpress/2024/01/03/three-kings-dance-celebration-features-lisa-botalicos-students/#more-73487"
+                href="http://www.towntopics.com/wordpress/2015/10/14/ole-flamenco-flourishes-in-princeton-as-lisa-botalico-leads-eager-spanish-dancers/"
                 alt="towntopics"
                 target="blank"
                 className="press"
               >
                 {" "}
-                Three Kings Dance Celebration Features Lisa Botalico’s Students
+                “Ole!” Flamenco Flourishes, in Princeton As Lisa Botalico Leads
+                Eager Spanish Dancers
               </a>
               <br />
               Princeton Town Topics
-            </p>
+            </h6>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
             <h3>US1</h3>
-            <p className="larger">
+            <h6 className="larger">
               <Link
                 to={US1Article}
                 alt="US1 article"
@@ -45,7 +104,45 @@ const Reviews = () => {
               </Link>
               <br />
               US1 interviews Lisa about flamenco and Paco Pena
-            </p>
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
+            <h3>
+              Princeton Town Topics, 2024 <small>(link)</small>
+            </h3>
+            <h6 className="larger">
+              <a
+                href="https://www.towntopics.com/wordpress/2024/01/03/three-kings-dance-celebration-features-lisa-botalicos-students/#more-73487"
+                alt="towntopics"
+                target="blank"
+                className="press"
+              >
+                {" "}
+                Three Kings Dance Celebration Features Lisa Botalico’s Students
+              </a>
+              <br />
+              Princeton Town Topics
+            </h6>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item style={carouselItemStyle}>
+          <Carousel.Caption>
+            <h3>US1</h3>
+            <h6 className="larger">
+              <Link
+                to={US1Article}
+                alt="US1 article"
+                target="blank"
+                className="press"
+              >
+                {" "}
+                US1 Interview
+              </Link>
+              <br />
+              US1 interviews Lisa about flamenco and Paco Pena
+            </h6>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
@@ -53,7 +150,7 @@ const Reviews = () => {
           2024
           <Carousel.Caption>
             <h3>Princeton Town Topics</h3>
-            <p className="larger">
+            <h6 className="larger">
               <Link
                 to={RobesonArticle}
                 alt="Paule Robeson 126 birthday celebration"
@@ -66,7 +163,7 @@ const Reviews = () => {
               <br />
               The Arts Council of Princeton Celebrates 126th Birthday of Paul
               Robeson
-            </p>
+            </h6>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
@@ -74,7 +171,7 @@ const Reviews = () => {
             <h3>
               Princeton Town Topics, 2015 <small>(link)</small>
             </h3>
-            <p className="larger">
+            <h6 className="larger">
               <a
                 href="http://www.towntopics.com/wordpress/2015/10/14/ole-flamenco-flourishes-in-princeton-as-lisa-botalico-leads-eager-spanish-dancers/"
                 alt="towntopics"
@@ -87,13 +184,13 @@ const Reviews = () => {
               </a>
               <br />
               Princeton Town Topics
-            </p>
+            </h6>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item style={carouselItemStyle}>
           <Carousel.Caption>
             <h3>US1</h3>
-            <p className="larger">
+            <h6 className="larger">
               <Link
                 to={US1Article}
                 alt="US1 article"
@@ -105,104 +202,7 @@ const Reviews = () => {
               </Link>
               <br />
               US1 interviews Lisa about flamenco and Paco Pena
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <h3>New York Times</h3>
-            <p className="larger">
-              "Another notable solo was Lisa Botalico's <em>Solea</em> which,
-              began as a lamentation and proceeded into a dance of pride and
-              defiance."
-              <em>
-                <br />
-                (Jack Anderson, NY Times)
-              </em>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <p className="larger">
-              "Botalico passed through a variety of rhythms and moods in her
-              solo <em>Cantinas.</em>...she emphasized the swaying of her hips,
-              the curling tracery of her arms, starkly dramatic poses and a
-              rapid 'zapateado.' Finally, hitching up her skirts, she became a
-              saucy one woman fiesta."
-              <em>(Star Ledger)</em>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <p className="larger">
-              "Lisa Botalico was the essence of fiery passion, first playing the
-              role of a slyly sultry seductress, then becoming the
-              personification of a flickering flame."{" "}
-              <em>(Asbury Park Press)</em>
-              <br />
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <h3>Il Progresso</h3>
-            <p className="larger">
-              "Bravo Lisa Bottalico, in an applauditissimo <em>Solea</em>; she
-              made the stage tremble."
-              <br />
-              <em>(Mario Fratti)</em>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <p className="larger">
-              "...Lisa Botalico's <em>Solea</em> had the crowd on it's feet,
-              clapping in rhythm because of the audacious display of drama
-              emanating from her being." <em>(Asbury Park Press)</em>
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <h3>
-              Princeton Town Topics <small>(link)</small>
-            </h3>
-            <p className="larger">
-              <a
-                href="http://www.towntopics.com/wordpress/2015/10/14/ole-flamenco-flourishes-in-princeton-as-lisa-botalico-leads-eager-spanish-dancers/"
-                alt="towntopics"
-                target="blank"
-                className="press"
-              >
-                {" "}
-                “Ole!” Flamenco Flourishes, in Princeton As Lisa Botalico Leads
-                Eager Spanish Dancers
-              </a>
-              <br />
-              Princeton Town Topics
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item style={carouselItemStyle}>
-          <Carousel.Caption>
-            <h3>US1</h3>
-            <p className="larger">
-              <Link
-                to={US1Article}
-                alt="US1 article"
-                target="blank"
-                className="press"
-              >
-                {" "}
-                US1 Interview
-              </Link>
-              <br />
-              US1 interviews Lisa about flamenco and Paco Pena
-            </p>
+            </h6>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
